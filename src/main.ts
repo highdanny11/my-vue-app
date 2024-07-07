7c528f6bd5cac12f7952fa779d003f3eea28c61d
BL001 <>
2024-07-07 14:12:57 +0800

    測試

--- src/main.ts
+++ src/main.ts
@@ -2,14 +2,6 @@ import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import App from './App.vue'
 
-if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('/sw.js', { scope: '/' })
-    .then(
-      () => console.log('register success'),
-      (err) => console.log('register fail:', err)
-    )
-}
-
 const pinia = createPinia()
 const app = createApp(App)
 app.use(pinia)
