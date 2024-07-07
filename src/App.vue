6569569fe9eca247adf9227f6168cb8a13c41f9b
BL001 <>
2024-07-07 16:46:52 +0800

    測試

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,22 @@
 <template>
   <div class="test">
-    title
-  <img src="https://cdn.lioshutan.com/arts/recruitment/category1.png" alt="">
+    <img :src="imgData" alt="">
   </div>
+  <router-view></router-view>
 </template>
 
 <script setup lang="ts">
+import pdfRender from '@/utils/pdfRender'
+import { onMounted, ref } from 'vue'
+const imgData = ref('')
 
+
+
+onMounted(async () => {
+  const res = await pdfRender()
+  imgData.value = res
+  console.log(res)
+})
 </script>
 
 <style scoped>
--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,7 @@
 /// <reference types="vite/client" />
+declare module '@/assets/pdf.worker.min.mjs'
+declare module "*.vue" {
+  import type { DefineComponent } from "vue";
+  const component: DefineComponent<{}, {}, any>
+  export default component
+}
